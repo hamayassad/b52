cfd88f92d8ed3cde3bc023528d8877854b7b144e
hamayassad <82609440+hamayassad@users.noreply.github.com>
2021-04-15 21:56:32 +0300

    Add files via upload

---
+++ termuxx.py
@@ -0,0 +1,18 @@
+apt update
+apt upgrade
+apt install figlet -y
+figlet Basic Installation
+apt install toilet -y
+apt install cowsay -y
+apt install nano -y
+apt install ruby -y
+apt install python -y
+apt install python2 -y
+apt install git -y
+apt install openssh -y
+gem install lolcat
+figlet -f big Done !!! | lolcat
+echo
+echo -e "\e[1m Now Run bash logo.sh...!!!"
+echo
+echo -e "\e[1m\e[32m Developed by :\e[33m Hama Crack"
